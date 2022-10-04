769717581ef8630b5d72bfbdd7c815fd4bcc0bb5
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-05 02:22:39 +0300

    05_10_4

---
+++ 05_10_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет
+//является ли одно исло квадратом другого.
+
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b ^ b == a*a)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+      Console.WriteLine("нет");
+}
+

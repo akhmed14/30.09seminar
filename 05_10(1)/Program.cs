dfe522f3e86833e609605b3b719dbbd52aa84f69
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-05 01:29:21 +0300

    05_10(1)

---
+++ 05_10(1)/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное трехзнаечное число и удаляет вторую цифру этого числа.
+
+
+int num = new Random().Next(100,1000);
+int a1 = num/100;
+int a3 = num%10;
+Console.WriteLine(num);
+Console.WriteLine($"{a1}{a3}");

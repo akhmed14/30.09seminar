d542634f79b6ff8159fb9dc1f781418ebb911351
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-05 02:11:35 +0300

    05_10_3

---
+++ 05_10_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли
+//оно одноврменно 7 и 23.
+
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+if(a%7 == 0 & a%23 == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+      Console.WriteLine("нет");
+}

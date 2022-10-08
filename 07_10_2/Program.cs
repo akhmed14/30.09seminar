fe61d66a75ab82b1014d6dbeb0647acff59ab21e
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-09 02:05:57 +0300

    07 10 2

---
+++ 07_10_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая по
+//заданному номеру четверти, показывает диапазон
+//возможных координат точек в этой четверти (x и y).
+Console.Write("Введите номер четверти ");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+ {
+    case 1: 
+    Console.WriteLine("X > 0 и Y > 0"); 
+    break;
+    case 2: 
+    Console.WriteLine("X < 0 и Y > 0"); 
+    break;
+    case 3: 
+    Console.WriteLine("X < 0 и Y < 0"); 
+    break;
+    case 4: 
+    Console.WriteLine("X > 0 и Y < 0"); 
+    break;
+
+    default: Console.WriteLine("Ошибка"); break;
+ }
+

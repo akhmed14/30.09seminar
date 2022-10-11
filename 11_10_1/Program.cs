c953d3c35965e7baead1318bb521d5984152cd64
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-11 20:21:46 +0300

    11_10_1

---
+++ 11_10_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Ввелите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");    
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num1)}");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num2)}");
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+  
+        sum = sum + number;
+        number--;
+    }
+    return sum;
+}

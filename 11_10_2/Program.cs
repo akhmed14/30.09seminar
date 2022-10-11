1d5661ed0e4759272c58dfc674c497a6fdd06594
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-11 20:44:24 +0300

    11_10_2

---
+++ 11_10_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+Console.Write("Ввелите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел  = {GetMultNums(num)}");    
+
+
+int GetMultNums(int number)
+{
+    int Mult = 1;
+    while(number>0)
+    {
+  
+        Mult = Mult * number;
+        number--;
+    }
+    return Mult;
+}

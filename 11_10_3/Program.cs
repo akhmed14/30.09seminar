d9c78f0a0f58627219ec0bba5f1c411b9057c45e
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-11 20:44:36 +0300

    11_10_3

---
+++ 11_10_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Ввелите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Кол-во цифр в числе  = {GetSumNums(num)}");   
+
+
+int GetSumNums(int number)
+{
+    int i = 0;
+    while(num!=0)
+    {
+        num = num/10;
+       
+        i++;
+    }
+    return i;
+}
+
+
+//int n = 123456789; 
+//int digitCount = (int)Math.Log10(n) + 1
